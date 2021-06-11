27cc9e586791205eeeec166c5217c9d1ec8fe2c8
Sanaaggarwal <72506943+Sanaaggarwal@users.noreply.github.com>
2021-06-11 18:05:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+nose_x=0;
+nose_y=0;
+function preload(){
+    img=loadImage("l1.png");
+}
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on('pose',getposes);
+}
+function takesnapshot(){
+save("myfilter.png");
+}
+function modelloaded(){
+    console.log("posenet is loaded");
+}
+function getposes(results){
+    if(results.length>0){
+        console.log(results);
+        nose_x=results[0].pose.nose.x;
+        nose_y=results[0].pose.nose.y;
+        console.log("nose x="+nose_x);
+        console.log("nose y="+nose_y);
+    }
+}
+function draw(){
+    image(video,0,0,400,400);
+    /*
+    fill("red");
+    stroke("yellow");
+    circle(nose_x,nose_y,20);
+    */
+   image(img,nose_x-25,nose_y+25,50,30);
+}
